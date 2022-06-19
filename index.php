dca80854ed36348e66b49174a6ac32ebd66a988a
Ahmadjon Dadabayev <98649872+Akhmadjonuz@users.noreply.github.com>
2022-06-20 02:26:05 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,5 @@
 <?php
 
-# Mualliflik huquqi saqlansin!
-# dadabayev.uz
-# +998902224311
-# telegram: @Akhmadjon.
-
-# Copyright reserved!
-# dadabayev.uz
-# +998902224311
-# telegram: @Akhmadjon.
-
-# Авторские права защищены!
-# dadabayev.uz
-# +998902224311
-# telegram: @Akhmadjon.
-
 function convert($name, $image)
 {
     $url  = 'https://cartoon.lyrebirdstudio.net/v1/process';
